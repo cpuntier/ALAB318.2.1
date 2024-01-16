1ed6aa7eb87bd8e2b24eb9d2b9cb409dd46f3f03
cpuntier <cpuntier@gmail.com>
2024-01-15 20:10:09 -0500

    download works

--- index.js
+++ index.js
@@ -9,29 +9,56 @@ const str = "hello";
 
 app.set('view engine', 'ejs');
 
+const helper = require("./helper");
 
+
+app.use(express.urlencoded({extended : true}));
 //ejs.render(str, data, options);
 
-const logger = (req,res,next) => {
+const logger = (req, res, next) => {
     console.log(`Request was made at :${req.method}${req.url}`);
     next();
 }
 
+
+
+
+
 app.use(logger);
+app.use(express.static("./images"));
+app.use(express.static("./"));
 
 //templates wip
 app.get('/', (req, res) => {
     const content = "Hello this is me"
     res.render('pages/index', {
         content: content,
-      });
     });
-
-app.get('/about', function (req, res) {
-    res.render('pages/about');
 });
 
+app.get('/about', (req, res) => {
+    const img = 'img.jpg';
+    const show = () => {
+        console.log("Hello")
+    }
+    const showFunc = show;
+
+
+    res.render('pages/about', {
+        img: img,
+        helper:helper,
+    });
+});
+
+app.post('/about', (req,res) => {
+    res.download("./images/img.jpg");
+});
 
 app.listen(PORT, () => {
     console.log("The server has been fired");
 });
+
+
+function show(){
+    console.log("hello");
+}
